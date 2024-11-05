667ff12facd92ae544848778b6ab644b1fb4669a
nnminh-sam <nnminh.sam.1803@gmail.com>
2024-11-05 17:07:49 +0700

    fix: added dotenv for retrieving JWT token config from env

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -5,6 +5,8 @@ import { UserModule } from 'src/user/user.module';
 import { JwtModule } from '@nestjs/jwt';
 import { LocalStrategy } from './strategies/local.strategy';
 import { JwtStrategy } from './strategies/jwt.strategy';
+import * as dotenv from 'dotenv';
+dotenv.config();
 
 @Module({
   imports: [
